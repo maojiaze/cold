0db9440eb2e923c21449a941c488d68916c46848
zzh <xyxx977@126.com>
2020-09-15 15:31:29 +0800

    m

--- src/main/java/com/example/cold/ColdProjectApplication.java
+++ src/main/java/com/example/cold/ColdProjectApplication.java
@@ -1,4 +1,4 @@
-package com.example.cold_project;
+package com.example.cold;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/cold/ColdProjectApplicationTests.java
+++ src/test/java/com/example/cold/ColdProjectApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.cold_project;
+package com.example.cold;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
