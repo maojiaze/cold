b8e79e3eafceba0a809f8c637491a91151e7f956
zzh <xyxx977@126.com>
2020-09-15 14:49:45 +0800

    1

---
+++ src/main/java/com/example/cold_project/ColdProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.cold_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColdProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColdProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cold_project/ColdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cold_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColdProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
